0c9cd3772224b35ef44fb6aff1bc76368413b05a
Richardt Justke <rj.justke@gmail.com>
2026-08-20 17:19:51 -0300

    feat:theme and prompt selected and ready

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,2 +1,41 @@
 package message
 
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+var temas = []string{
+	"superação",
+	"foco",
+	"gratidão",
+	"humor",
+	"disciplina",
+	"determinação",
+	"sucesso",
+	"coragem",
+	"recomeço",
+	"persistência",
+	"autoconfiança",
+	"sonhos",
+	"mudança",
+	"resiliência",
+	"propósito",
+	"crescimento pessoal",
+	"conquista",
+}
+
+const promptTemplate = "Gere uma mensagem motivacional curta sobre %s, em português, tom inspirador."
+
+func randTema(temas []string) string {
+	indice := rand.IntN(len(temas))
+	sorteado := temas[indice]
+	return sorteado
+}
+func temaPrompt() string {
+	temaEscolhido := randTema(temas)
+	promptFinal := fmt.Sprintf(promptTemplate, temaEscolhido)
+
+	return promptFinal
+
+}
